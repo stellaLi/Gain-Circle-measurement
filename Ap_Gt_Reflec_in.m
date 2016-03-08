6a61ffbf97b5d999a304d6620e1361d428f6dd2d
stellaLi <stellaLi@users.noreply.github.com>
2016-03-08 17:56:37 +0100

    Added files via upload

---
+++ Ap_Gt_Reflec_in.m
@@ -0,0 +1,8 @@
+%
+% Measure transducer gain of a Rf Amp, as a function of source impedence
+% S21 seen from VNA the gain seen by VNA
+% S33 source impedence looked from the Amp side
+%
+% Sitian LI
+% 2016.03.08
+%
